0e329e0c4965718c582d936e2d83e922812e4c32
RishitSaxena55 <rishitsaxena55@gmail.com>
2025-06-16 16:59:06 +0530

    Add files via upload

---
+++ model.py
@@ -0,0 +1,42 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+class InvertedPyramidNet(nn.Module):
+    def __init__(self, input_dim, output_dim, dropout=0.25):
+        super(InvertedPyramidNet, self).__init__()
+
+        self.model = nn.Sequential(
+            nn.BatchNorm1d(input_dim),
+            nn.Linear(input_dim, 2048),
+            nn.BatchNorm1d(2048),
+            nn.Softplus(),
+            nn.Dropout(dropout),
+
+            nn.Linear(2048, 1024),
+            nn.BatchNorm1d(1024),
+            nn.Softplus(),
+            nn.Dropout(dropout),
+
+            nn.Linear(1024, 512),
+            nn.BatchNorm1d(512),
+            nn.Softplus(),
+            nn.Dropout(dropout),
+
+            nn.Linear(512, 256),
+            nn.BatchNorm1d(256),
+            nn.Softplus(),
+            nn.Dropout(dropout),
+
+            nn.Linear(256, output_dim)
+        )
+
+        # Xavier Initialization
+        for m in self.model:
+            if isinstance(m, nn.Linear):
+                nn.init.xavier_normal_(m.weight)
+                if m.bias is not None:
+                    nn.init.zeros_(m.bias)
+
+    def forward(self, x):
+        return self.model(x)
